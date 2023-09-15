d9cf0a013f9fe332b6beb0b2abfc4a983693ce2c
josejustino <josejustinno@gmail.com>
2023-09-15 13:21:57 -0300

    chore: set jest to pass with no test

--- jest.config.ts
+++ jest.config.ts
@@ -7,11 +7,11 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  passWithNoTests: true,
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
