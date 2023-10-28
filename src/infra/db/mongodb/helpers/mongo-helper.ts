ba2096565382067d02da4388df0226f4b0a6be1d
josejustino <josejustinno@gmail.com>
2023-10-27 21:39:45 -0300

    refactor: move mongodb methods to a helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,11 +2,12 @@ import { type Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri)
   },
 
-  async disconnect () {
+  async disconnect (): Promise<void> {
     await this.client.close()
   },
 
