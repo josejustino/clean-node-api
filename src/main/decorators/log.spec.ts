5c290d0ff17e0fe6280d695ecf5ab649b32b2589
josejustino <josejustinno@gmail.com>
2023-10-29 13:29:04 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -53,4 +53,28 @@ describe('LogController Decorator', () => {
 
     expect(handleSpy).toHaveBeenLastCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'José',
+        email: 'josejustinno@gmail.com',
+        password: '12345',
+        passwordConfirmation: '12345'
+      }
+    })
+  })
 })
