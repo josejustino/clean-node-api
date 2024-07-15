761878146f30c26d8b320b7b8e6d8d1097f10d57
josejustino <josejustinno@gmail.com>
2024-07-15 10:04:58 -0300

    refactor: move EmailValidation to ValidationComposite

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,5 +4,5 @@ import env from './config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import ('./config/app')).default
-    app.listen(env.port, () => { console.log('Server running at http://localhost:5050') })
+    app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
   }).catch(console.error)
--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -13,11 +13,9 @@ export class EmailValidation implements Validation {
 
   validate (input: any): Error | null {
     const isValid = this.emailValidator.isValid(input[this.fieldName])
-
     if (!isValid) {
       return new InvalidParamError(this.fieldName)
     }
-
     return null
   }
 }
