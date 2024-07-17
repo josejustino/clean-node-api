3cd66f64305dbcdb6f6b634e10c0d6f3381f4a0a
josejustino <josejustinno@gmail.com>
2024-07-17 10:37:43 -0300

    refactor: rename update with updateAccessToken

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -27,7 +27,7 @@ export class DbAuthentication implements Authentication {
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
         if (accessToken) {
-          await this.updateAccessTokenRepository.update(account.id, accessToken)
+          await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
           return accessToken
         }
       }
