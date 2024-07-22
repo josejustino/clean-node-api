b8dc14fe84c978d8f7d7bc0841cb5b9d6cd720e9
josejustino <josejustinno@gmail.com>
2024-07-22 10:38:46 -0300

    test: ensure SignUpController calls authentication with correct values

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -4,6 +4,7 @@ import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 import { type AddAccount, type AddAccountModel } from '../../../domain/usecases/add-account'
 import { type AccountModel } from '../../../domain/models/account'
 import { type HttpRequest, type Validation } from '../../protocols'
+import { type Authentication, type AuthenticationParams } from '../../../domain/usecases/authentication'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -11,8 +12,16 @@ const makeAddAccount = (): AddAccount => {
       return await Promise.resolve(makeFakeAccount())
     }
   }
-
   return new AddAccountStub()
+}
+
+const makeAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (params: AuthenticationParams): Promise<string> {
+      return 'any_token'
+    }
+  }
+  return new AuthenticationStub()
 }
 
 const makeValidation = (): Validation => {
@@ -21,7 +30,6 @@ const makeValidation = (): Validation => {
       return null
     }
   }
-
   return new ValidationStub()
 }
 
@@ -45,40 +53,36 @@ interface SutTypes {
   sut: SignUpController
   addAccountStub: AddAccount
   validationStub: Validation
+  authenticationStub: Authentication
 }
 
 const makeSut = (): SutTypes => {
   const addAccountStub = makeAddAccount()
   const validationStub = makeValidation()
-  const sut = new SignUpController(addAccountStub, validationStub)
-
+  const authenticationStub = makeAuthentication()
+  const sut = new SignUpController(addAccountStub, validationStub, authenticationStub)
   return {
     sut,
     addAccountStub,
-    validationStub
+    validationStub,
+    authenticationStub
   }
 }
 
 describe('SignUp Controller', () => {
   test('Should return 500 if AddAccount throws', async () => {
     const { sut, addAccountStub } = makeSut()
-
     jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => {
       return await Promise.reject(new Error())
     })
-
     const httpResponse = await sut.handle(makeFakeRequest())
-
     expect(httpResponse).toEqual(serverError(new ServerError()))
   })
 
   test('Should call AddAccount with correct values', async () => {
     const { sut, addAccountStub } = makeSut()
-
     const addSpy = jest.spyOn(addAccountStub, 'add')
-
     await sut.handle(makeFakeRequest())
-
     expect(addSpy).toHaveBeenCalledWith({
       name: 'any_name',
       email: 'any_email@mail.com',
@@ -88,30 +92,32 @@ describe('SignUp Controller', () => {
 
   test('Should return 200 if valid data is provided', async () => {
     const { sut } = makeSut()
-
     const httpResponse = await sut.handle(makeFakeRequest())
-
     expect(httpResponse).toEqual(ok(makeFakeAccount()))
   })
 
   test('Should call Validation with correct value', async () => {
     const { sut, validationStub } = makeSut()
-
     const validateSpy = jest.spyOn(validationStub, 'validate')
     const httpRequest = makeFakeRequest()
-
     await sut.handle(httpRequest)
-
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
   test('Should return 400 if validation returns an error', async () => {
     const { sut, validationStub } = makeSut()
-
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
-
     const httpResponse = await sut.handle(makeFakeRequest())
-
     expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
+    await sut.handle(makeFakeRequest())
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
   })
 })
