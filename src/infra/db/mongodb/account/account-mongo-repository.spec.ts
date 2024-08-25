ff58f0ab84e7d4d39da5f4b72906c95809fa107d
josejustino <josejustinno@gmail.com>
2024-08-25 11:39:19 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -16,7 +16,6 @@ describe('Account MongoRepository', () => {
 
   beforeEach(async () => {
     accountCollection = MongoHelper.getCollection('accounts')
-
     await accountCollection.deleteMany({})
   })
 
@@ -97,6 +96,24 @@ describe('Account MongoRepository', () => {
       expect(account?.id).toBeTruthy()
       expect(account?.name).toBe('any_name')
       expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
+    })
+
+    test('Should return an account on loadByToken with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
       expect(account?.password).toBe('any_password')
     })
   })
