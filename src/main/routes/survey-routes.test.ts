df3a83a06ed9d21f6a0be23622fc3ba8a937792f
josejustino <josejustinno@gmail.com>
2024-09-07 12:24:06 -0300

    test: ensure load surveys route returns 200 if a valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -80,5 +80,42 @@ describe('Survey Routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+
+    test('Should return 200 on load surveys with valid accessToken', async () => {
+      const password = await hash('123456', 12)
+      const res = await accountCollection.insertOne({
+        name: 'José',
+        email: 'jose.justinno@gmail.com',
+        password
+      })
+      const { insertedId: id } = res
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      },
+      {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken).expect(200)
+    })
   })
 })
