548ba21092e8b5c1db258c7588c2a32138073396
josejustino <josejustinno@gmail.com>
2024-10-28 20:28:06 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -70,5 +70,26 @@ describe('Account MongoRepository', () => {
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(survey.answers[0].answer)
     })
+
+    test('Should update survey result if its not new', async () => {
+      const survey = await makeSurvey()
+      const accountId = await makeAccountId()
+      const res = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toEqual(res.insertedId.toHexString())
+      expect(surveyResult.answer).toBe(survey.answers[1].answer)
+    })
   })
 })
